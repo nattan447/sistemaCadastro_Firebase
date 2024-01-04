d281cfab714af6c0dfd15038a548f23475174b3c
nattan447 <nattanferreira58@gmail.com>
2024-01-04 15:57:12 -0300

    rmovi dados sensiveis

--- FirebaseConfig.ts
+++
@@ -1,9 +0,0 @@
-const FirebaseConfig = {
-  apiKey: 'AIzaSyAhKcBCkKgwHOCSec6nOA83FVcxGOgIMoo',
-  authDomain: 'loginscreen-56dd2.firebaseapp.com',
-  projectId: 'loginscreen-56dd2',
-  storageBucket: 'loginscreen-56dd2.appspot.com',
-  messagingSenderId: '89530609566',
-  appId: '1:89530609566:web:84c0d7df333fc004ddf08f',
-};
-export {FirebaseConfig};
